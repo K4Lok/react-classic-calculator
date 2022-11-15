a411affa086da59a565464e766ca088994189ed3
K4Lok <samkalok107@gmail.com>
2022-11-16 02:31:00 +0800

    feature: overflow srcoll for sum value, auto scrolling for long value

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import './App.css'
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Operations } from "./utils/Operations";
 
 function App() {
@@ -8,6 +8,8 @@ function App() {
   const [shouldClearSum, setShouldClearSum] = useState(false);
   const [isOperating, setIsOperating] = useState(false);
   const [operationType, setOperationType] = useState(null);
+
+  const sumElement = useRef();
 
   const numberPress = (e) => {
     const value = e.target.dataset.value;
@@ -27,6 +29,8 @@ function App() {
     else {
       setSum(prev => parseFloat(`${prev}${value}`));
     }
+
+    sumElement.current.scrollLeft = sumElement.current.clientWidth;
   }
   
   const resetPress = () => {
@@ -73,6 +77,8 @@ function App() {
     setOperationType(null);
     setIsOperating(false);
     setTempSum(null);
+
+    sumElement.current.scrollLeft = -sumElement.current.clientWidth;
   }
 
   const addDot = () => {
@@ -84,12 +90,12 @@ function App() {
       <div className="flex justify-center items-center h-screen bg-slate-500">
         <div className="max-w-5xl mx-6 md:mx-auto bg-gray-200 py-6 px-8 rounded-3xl shadow-2xl overflow-x-hidden">
           <h1 className="text-4xl md:text-5xl font-bold font-mono mb-4 text-center">Calculator</h1>
-          <div className="relative overflow-hidden">
+          <div className="relative"> 
             <img 
               src="https://images.unsplash.com/photo-1614850715649-1d0106293bd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
               className="w-96 h-16 object-fit rounded-md opacity-60"
               />
-            <p className="absolute top-1/2 right-0 pr-5 -translate-y-1/2 text-5xl text-slate-800 font-bold">{sum}</p>
+            <p ref={sumElement} className="absolute top-1/2 right-0 left-0 p-5 -translate-y-1/2 text-5xl text-slate-800 text-end font-bold overflow-x-scroll overflow-y-hidden scrollbar-hide">{sum}</p>
             </div>
             <div className="grid grid-cols-4 text-center align-center my-4 gap-x-4 gap-y-4 md:gap-x-6 md:gap-y-4">
               <div className="cal-btn" onClick={resetPress} data-value="AC">AC</div>
