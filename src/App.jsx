08e8cf9088a1dbb6e7007f7076f64d6cdd3bc1e6
K4Lok <samkalok107@gmail.com>
2022-11-16 01:57:34 +0800

    fix: padding on sm screen device

--- src/App.jsx
+++ src/App.jsx
@@ -81,18 +81,17 @@ function App() {
 
   return (
     <div className="App">
-      <div className="flex justify-center items-center min-h-screen bg-slate-500">
-        <div className="max-w-5xl mx-6 md:mx-auto bg-gray-200 py-6 px-8 rounded-3xl shadow-2xl">
-          <h1 className="text-5xl font-bold font-mono mb-4 text-center">Calculator</h1>
+      <div className="flex justify-center items-center h-screen bg-slate-500">
+        <div className="max-w-5xl mx-6 md:mx-auto bg-gray-200 py-6 px-8 rounded-3xl shadow-2xl overflow-x-hidden">
+          <h1 className="text-4xl md:text-5xl font-bold font-mono mb-4 text-center">Calculator</h1>
           <div className="relative overflow-hidden">
             <img 
               src="https://images.unsplash.com/photo-1614850715649-1d0106293bd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-              alt="" 
               className="w-96 h-16 object-fit rounded-md opacity-60"
               />
             <p className="absolute top-1/2 right-0 pr-5 -translate-y-1/2 text-5xl text-slate-800 font-bold">{sum}</p>
             </div>
-            <div className="grid grid-cols-4 text-center align-center my-4 gap-x-4 gap-y-2 md:gap-x-6 md:gap-y-3">
+            <div className="grid grid-cols-4 text-center align-center my-4 gap-x-4 gap-y-4 md:gap-x-6 md:gap-y-4">
               <div className="cal-btn" onClick={resetPress} data-value="AC">AC</div>
               <div className="cal-btn" onClick={reverseSign} data-value="+/-">+/-</div>
               <div className="cal-btn" onClick={operationPress} data-value="%">%</div>
